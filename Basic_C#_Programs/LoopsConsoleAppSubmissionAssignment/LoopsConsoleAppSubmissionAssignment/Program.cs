7c29d4b3b037a70239c52dc623a2d4f3e13f8477
Michaela Rehm <112276652+Michaelar1@users.noreply.github.com>
2023-05-04 16:27:23 -0400

    Fixed bug in LoopsConsoleAppSubmissionAssignment

--- Basic_C#_Programs/LoopsConsoleAppSubmissionAssignment/LoopsConsoleAppSubmissionAssignment/Program.cs
+++ Basic_C#_Programs/LoopsConsoleAppSubmissionAssignment/LoopsConsoleAppSubmissionAssignment/Program.cs
@@ -24,63 +24,66 @@ namespace LoopsConsoleAppSubmissionAssignment
             int num = Convert.ToInt32(Console.ReadLine());  // Stores number as integer variable num
             do  // Do-While Loop: While the number picked is not 9, the program continues to run to let the user guess another number.
             {
-                case 1:
-                    Console.WriteLine("You chose 1. That is not the correct number. Try again!");
-                    Console.WriteLine("Pick a number from 1-10!");
-                    int num = Convert.ToInt32(Console.ReadLine());
-                    break;
-                case 2:
-                    Console.WriteLine("You chose 2. That is not the correct number. Try again!");
-                    Console.WriteLine("Pick a number from 1-10!");
-                    int num = Convert.ToInt32(Console.ReadLine());
-                    break;
-                 case 3:
-                    Console.WriteLine("You chose 3. That is not the correct number. Try again!");
-                    Console.WriteLine("Pick a number from 1-10!");
-                    int num = Convert.ToInt32(Console.ReadLine());
-                    break;
-                 case 4:
-                    Console.WriteLine("You chose 4. That is not the correct number. Try again!");
-                    Console.WriteLine("Pick a number from 1-10!");
-                    int num = Convert.ToInt32(Console.ReadLine());
-                    break;
-                 case 5:
-                    Console.WriteLine("You chose 5. That is not the correct number. Try again!");
-                    Console.WriteLine("Pick a number from 1-10!");
-                    int num = Convert.ToInt32(Console.ReadLine());
-                    break;
-                 case 6:
-                    Console.WriteLine("You chose 6. That is not the correct number. Try again!");
-                    Console.WriteLine("Pick a number from 1-10!");
-                    int num = Convert.ToInt32(Console.ReadLine());
-                    break;
-                 case 7:
-                    Console.WriteLine("You chose 7. That is not the correct number. Try again!");
-                    Console.WriteLine("Pick a number from 1-10!");
-                    int num = Convert.ToInt32(Console.ReadLine());
-                    break;
-                 case 8:
-                    Console.WriteLine("You chose 8. That is not the correct number. Try again!");
-                    Console.WriteLine("Pick a number from 1-10!");
-                    int num = Convert.ToInt32(Console.ReadLine());
-                    break;
-                 case 9:
-                    Console.WriteLine("You chose 9. You guessed correctly! You win!");
-                    break;
-                 case 10:
-                    Console.WriteLine("You chose 10. That is not the correct number. Try again!");
-                    Console.WriteLine("Pick a number from 1-10!");
-                    int num = Convert.ToInt32(Console.ReadLine());
-                    break;
-                default:
-                    Console.WriteLine("You did not choose a number from 1-10. Try again!");
-                    Console.WriteLine("Pick a number from 1-10!");
-                    int num = Convert.ToInt32(Console.ReadLine());
-                    break;
+                switch (num)
+                {
+                    case 1:
+                        Console.WriteLine("You chose 1. That is not the correct number. Try again!");
+                        Console.WriteLine("Pick a number from 1-10!");
+                        num = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 2:
+                        Console.WriteLine("You chose 2. That is not the correct number. Try again!");
+                        Console.WriteLine("Pick a number from 1-10!");
+                        num = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 3:
+                        Console.WriteLine("You chose 3. That is not the correct number. Try again!");
+                        Console.WriteLine("Pick a number from 1-10!");
+                        num = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 4:
+                        Console.WriteLine("You chose 4. That is not the correct number. Try again!");
+                        Console.WriteLine("Pick a number from 1-10!");
+                        num = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 5:
+                        Console.WriteLine("You chose 5. That is not the correct number. Try again!");
+                        Console.WriteLine("Pick a number from 1-10!");
+                        num = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 6:
+                        Console.WriteLine("You chose 6. That is not the correct number. Try again!");
+                        Console.WriteLine("Pick a number from 1-10!");
+                        num = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 7:
+                        Console.WriteLine("You chose 7. That is not the correct number. Try again!");
+                        Console.WriteLine("Pick a number from 1-10!");
+                        num = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 8:
+                        Console.WriteLine("You chose 8. That is not the correct number. Try again!");
+                        Console.WriteLine("Pick a number from 1-10!");
+                        num = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 9:
+                        Console.WriteLine("You chose 9. You guessed correctly! You win!");
+                        break;
+                    case 10:
+                        Console.WriteLine("You chose 10. That is not the correct number. Try again!");
+                        Console.WriteLine("Pick a number from 1-10!");
+                        num = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    default:
+                        Console.WriteLine("You did not choose a number from 1-10. Try again!");
+                        Console.WriteLine("Pick a number from 1-10!");
+                        num = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
             }
             while (num != 9);
 
-            Console.ReadLine();
+            Console.ReadLine();  // Keeps window open unless the user takes another action.
         }
     }
 }
