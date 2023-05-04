9f8c633bb05cc5a72b6157dfe0ed034c5945977a
Michaela Rehm <112276652+Michaelar1@users.noreply.github.com>
2023-05-04 17:47:30 -0400

    Update ArraySubmissionAssignment
    Added Notes

--- Basic_C#_Programs/ArraySubmissionAssignment/ArraySubmissionAssignment/Program.cs
+++ Basic_C#_Programs/ArraySubmissionAssignment/ArraySubmissionAssignment/Program.cs
@@ -5,33 +5,33 @@ using System.Collections.Generic;
     {
     static void Main(string[] args)
     {
-        string[] stringArray = { "stingray", "Hello!", "puppies", "outside" };
+        string[] stringArray = { "stingray", "Hello!", "puppies", "outside" };  // Create a string array
 
-        Console.WriteLine("Please choose a number 0-3");
-        int stringIndex = Convert.ToInt32(Console.ReadLine());
-        if (stringIndex <= 3)
+        Console.WriteLine("Please choose a number 0-3");  // Prompts the user to choose the index from the array
+        int stringIndex = Convert.ToInt32(Console.ReadLine());  // Stores the number chosen as stringIndex
+        if (stringIndex <= 3)  // If the user chooses a valid number, the program returns the string at that index
         {
             Console.WriteLine(stringArray[stringIndex]);
         }
-        else
+        else  // If the user chooses an invalid number, the program throws an error message
         {
             Console.WriteLine("You did not choose a number between 0 and 3.");
         }
 
-        int[] intArray = { 5, 12, 9, 45, 42, 65, 89, 77 };
+        int[] intArray = { 5, 12, 9, 45, 42, 65, 89, 77 };  // Creates integer array
 
-        Console.WriteLine("Please Choose a number 0-7.");
-        int intIndex = Convert.ToInt32(Console.ReadLine());
-        if (intIndex <= 7)
+        Console.WriteLine("Please Choose a number 0-7.");  // Prompts the user to choose the index from the array
+        int intIndex = Convert.ToInt32(Console.ReadLine());  // Stores the number chosen as intIndex
+        if (intIndex <= 7)  // If the user chose a valid number, the program returns the number at that index
         {
             Console.WriteLine(intArray[intIndex]);
         }
-        else
+        else  // If the user chose an invalid number, the program throws an error message.
         {
             Console.WriteLine("You did not choose a number between 0 and 7.");
         }
 
-        List<string> stringList = new List<string>();
+        List<string> stringList = new List<string>();  // Create stringList and add values
         stringList.Add("Hello, there!");
         stringList.Add("Hi!");
         stringList.Add("Bonjour!");
@@ -40,18 +40,18 @@ using System.Collections.Generic;
         stringList.Add("Hej!");
         stringList.Add("Guten Tag!");
 
-        Console.WriteLine("Please choose a number 0-6");
-        int listIndex = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Please choose a number 0-6");  // Prompts the user to choose an index from the list
+        int listIndex = Convert.ToInt32(Console.ReadLine());  // Stores the number chosen as listIndex
 
-        if (listIndex <= 6)
+        if (listIndex <= 6)  // If the user chose a valid number, the program will return the string at that index
         {
             Console.WriteLine(stringList[listIndex]);
         }
-        else
+        else  // If the user chose an invalid number, the program will throw an error message
         {
             Console.WriteLine("You did not choose a number from 0 to 6");
         }
-        Console.ReadLine();
+        Console.ReadLine();  // Keeps the window open until the user takes another action.
         }
     }
 
