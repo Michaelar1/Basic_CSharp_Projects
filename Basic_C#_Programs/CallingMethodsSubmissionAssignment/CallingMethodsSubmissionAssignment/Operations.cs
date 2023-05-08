847e6dd8454209d809faa94937675b3fd35d3b3a
Michaela Rehm <112276652+Michaelar1@users.noreply.github.com>
2023-05-08 17:11:48 -0400

    Created CallingMethodsSubmissionAssignment

---
+++ Basic_C#_Programs/CallingMethodsSubmissionAssignment/CallingMethodsSubmissionAssignment/Operations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsSubmissionAssignment
+{
+    public class Operations  // Create Class Operations
+    {
+        public static int Adding(int x)  // Create addition method
+        {
+            return x + 5;
+        }
+
+        public static int Multiplying(int x)  // Create multiplication method
+        {
+            return x * 10;
+        }
+
+        public static int Subtract(int x)  // Create subtraction method
+        {
+            return x - 3;
+        }
+    }
+}
---
+++ Basic_C#_Programs/CallingMethodsSubmissionAssignment/CallingMethodsSubmissionAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input an integer: ");  // Prompts user to enter an integer
+            int input = Convert.ToInt32(Console.ReadLine());  // Stores integer as int variable input
+            Console.WriteLine(input + " plus five equals " + Operations.Adding(input));  // Call the Adding Operation method and print to console
+            Console.WriteLine(input + " times ten equals " + Operations.Multiplying(input));  // Call the Multiplying Operation method and print to console
+            Console.WriteLine(input + " minus three equals " + Operations.Subtract(input));  // Call the Subtract Operation method and print to console
+            Console.ReadLine();  // Keeps program open until user takes another action.
+          
+        }
+    }
+}
