7d7ceb63607bc9657e603ed541c12d65c681be9f
Michaela Rehm <112276652+Michaelar1@users.noreply.github.com>
2023-05-10 12:09:44 -0400

    Created MethodSubmissionAssignment1

---
+++ Basic_C#_Programs/MethodSubmissionAssignment1/MethodSubmissionAssignment1/Math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmissionAssignment1
+{
+    public class Math
+    {
+        public static void Adding(int x, int y)  // Create adding method
+        {
+            Console.WriteLine(5 + x);  // Adds 5 to first parameter and prints to console
+            Console.WriteLine(y);  // Prints second parameter
+        }
+    }
+}
---
+++ Basic_C#_Programs/MethodSubmissionAssignment1/MethodSubmissionAssignment1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmissionAssignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math.Adding(5, 8);  // Call Math.Adding Class, passing in parameters
+
+            int x = 9;
+            int y = 42;
+            Math.Adding(x, y);  // Call Math.Adding Class, specifying parameters by name
+            
+            Console.ReadLine();
+        }
+    }
+}
