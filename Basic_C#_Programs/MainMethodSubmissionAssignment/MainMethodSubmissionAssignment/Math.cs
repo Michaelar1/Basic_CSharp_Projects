90c38658cdcb9552354e149205046d22ef0d72af
Michaela Rehm <112276652+Michaelar1@users.noreply.github.com>
2023-05-10 11:45:59 -0400

    Created MainMethodSubmission Assignment, updated TwentyOne.suo

---
+++ Basic_C#_Programs/MainMethodSubmissionAssignment/MainMethodSubmissionAssignment/Math.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodSubmissionAssignment
+{
+    public class Math  // Create Class Math
+    {
+        public static int Adding(int x)  // Create addition method
+        {
+            return x + 73;
+        }
+        public static decimal Adding(decimal x)  // Create overload method of adding that uses decimal data types
+        {
+            return x + 73.37m;
+        }
+
+        public static int Adding(string input) // Create overload method of adding method that will take a string.
+        {
+            int x = Convert.ToInt32(input);
+            return x + 73;
+        }
+    }
+}
---
+++ Basic_C#_Programs/MainMethodSubmissionAssignment/MainMethodSubmissionAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int toAdd = 22;  // Create int toAdd
+            int intAddResult = Math.Adding(toAdd);  // Call Adding method from Math and pass in toAdd integer.
+            Console.WriteLine(intAddResult);  // Print result from this method to the console.
+
+            decimal decToAdd = 22.22m;  // Create decimal decToAdd
+            decimal decAddResult = Math.Adding(decToAdd);  // Call overload Adding method from Math and pass in decToAdd decimal.
+            Console.WriteLine(decAddResult);  // Print result from this overload method to the console.
+
+            string strToAdd = "22";  // Create string strToAdd
+            int strAddResult = Math.Adding(strToAdd);  // Calls overload Adding method from Math and pass in strToAdd string 
+            Console.Write(strAddResult);  // Print result from this overload method to the console
+
+            Console.ReadLine();  // Keeps program open until the user takes another action.
+        }
+    }
+}
