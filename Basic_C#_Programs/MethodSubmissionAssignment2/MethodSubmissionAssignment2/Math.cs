e366867c689e1eb071369c2c04d49041b84e4653
Michaela Rehm <112276652+Michaelar1@users.noreply.github.com>
2023-05-10 13:24:53 -0400

    Updated MethodSubissionAssignment1.suo and Created MethodSubmissionAssignment2

---
+++ Basic_C#_Programs/MethodSubmissionAssignment2/MethodSubmissionAssignment2/Math.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmissionAssignment2
+{
+    public class Math
+    {
+        public int Multiply(int x, int y=5)  // Created a class with two parameters in which one is optional
+        {
+            return x * y;  // Multiplies parameters
+        }
+    }
+}
---
+++ Basic_C#_Programs/MethodSubmissionAssignment2/MethodSubmissionAssignment2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmissionAssignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math obj = new Math();  // Instantiate Math Class as object
+            Console.WriteLine("Please enter an integer: ");  // Prompt user for an integer
+            int multX = Convert.ToInt32(Console.ReadLine());  // store integer as int multX
+            Console.WriteLine("Please either enter an integer or simply press enter to continue: ");  // Prompt user for another integer or to press enter to use default
+            try  // If user enters a second integer, it will pass both parameters to the method
+            {
+                int multY = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(obj.Multiply(multX, multY));
+                
+            }
+           catch  // Else, the method will accept the first parameter and use the default parameter as the second integer
+            {
+                Console.WriteLine(obj.Multiply(multX));
+            }
+     
+
+            Console.ReadLine();
+        }
+    }
+}
