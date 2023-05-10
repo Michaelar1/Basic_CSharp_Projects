65f209925912be8c8aca911689ec732a943f5eba
Michaela Rehm <112276652+Michaelar1@users.noreply.github.com>
2023-05-10 16:01:47 -0400

    Created MethodsAndObjectsSubmissionAssignment

---
+++ Basic_C#_Programs/MethodsAndObjectsSubmissionAssignment/MethodsAndObjectsSubmissionAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjectsSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();  // Instantiates new employee object
+            employee.FirstName = "Sample";  // Sets FirstName to "Sample"
+            employee.LastName = "Student";  // Sets LastName to "Student
+
+            employee.SayName();  // Calls SayName() method for new employee.
+
+            Console.ReadLine();
+        }
+    }
+}
