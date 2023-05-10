08b7a3dd7b8238f22f3f5544683cf02a9aee9256
Michaela Rehm <112276652+Michaelar1@users.noreply.github.com>
2023-05-10 15:17:53 -0400

    Created ClassSubmissionAssignment

---
+++ Basic_C#_Programs/ClassSubmissionAssignment/ClassSubmissionAssignment/Math.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmissionAssignment
+{
+    public static class Math  // Declare class as static
+    {
+        public static void Divide (int x)  // Creates a void method that inputs an integer and outputs that integer divided by 2
+        {
+            Console.WriteLine(x / 2);
+        }
+
+        public static void Addition(out int x, out int y)  // Create a method with output parameters
+        {
+            x = 42;
+            y = 9;
+            x += x;
+            y += y;
+            
+        }
+
+        public static int Divide(int x, int y)  // Overloads a method (Divide)
+        {
+            return x / y;
+        }
+    }
+}
---
+++ Basic_C#_Programs/ClassSubmissionAssignment/ClassSubmissionAssignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Please enter an integer: ");  // Prompts user for an integer
+            int dividend = Convert.ToInt32(Console.ReadLine());  // Saves integer to int variable dividend
+            Math.Divide(dividend);  // Calls Divide method and passes parameter variable Dividend
+
+            int x;
+            int y;
+            Math.Addition(out x,out y); // Passes x an
+            Console.WriteLine("x is now " + x);
+            Console.WriteLine("y is now " + y);
+            
+
+           
+            Console.WriteLine(Math.Divide(56, 2));  // Calls overload Divide Method
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
