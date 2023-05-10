ab907fab3b9cc33c4683c9856afd7522c8f44b0f
Michaela Rehm <112276652+Michaelar1@users.noreply.github.com>
2023-05-10 15:41:48 -0400

    Update Math.cs

--- Basic_C#_Programs/MethodSubmissionAssignment1/MethodSubmissionAssignment1/Math.cs
+++ Basic_C#_Programs/MethodSubmissionAssignment1/MethodSubmissionAssignment1/Math.cs
@@ -8,10 +8,10 @@ namespace MethodSubmissionAssignment1
 {
     public class Math
     {
-        public static void Adding(int x, int y)  // Create adding method
+        public static void Adding(int firstParameter, int secondParameter)  // Create adding method
         {
-            Console.WriteLine(5 + x);  // Adds 5 to first parameter and prints to console
-            Console.WriteLine(y);  // Prints second parameter
+            Console.WriteLine(5 + firstParameter);  // Adds 5 to first parameter and prints to console
+            Console.WriteLine(secondParameter);  // Prints second parameter
         }
     }
 }
