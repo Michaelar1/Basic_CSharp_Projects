77bfb3444a14c5c3c3c4dafd06b0c4bc0856f0ea
Michaela Rehm <112276652+Michaelar1@users.noreply.github.com>
2023-05-11 13:46:31 -0400

    Created OperatorSubmissionAssignment

---
+++ Basic_C#_Programs/OperatorsSubmissionAssignment/OperatorsSubmissionAssignment/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsSubmissionAssignment
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }  // Create FirstName Property
+        public string LastName { get; set; }  // Create LastName Property
+        public int ID { get; set; }  // Create ID property
+
+        public static bool operator== (Employee employee, Employee employee1)  // Overload == operator to compare employees by ID
+        {
+            if (employee.ID == employee1.ID)
+            {
+                 return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator!= (Employee employee, Employee employee1)  // Overload != operator to pair with == operator overload
+        {
+            if (employee.ID != employee1.ID)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ Basic_C#_Programs/OperatorsSubmissionAssignment/OperatorsSubmissionAssignment/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();  // Create new employee
+            employee.FirstName = "John";
+            employee.LastName = "Smith";
+            employee.ID = 111;
+
+            // Test false -- Create different employee
+            //Employee employee1 = new Employee(); 
+            //employee1.FirstName = "Jane";
+            //employee1.LastName = "Doe";
+            //employee1.ID = 222;
+
+            // Test true - Create same employee
+            Employee employee1 = new Employee();
+            employee1.FirstName = "Jon";  // Changed spelling of first name to make sure program is comparing just the ID
+            employee1.LastName = "Smith";
+            employee1.ID = 111;
+
+            if (employee == employee1)  // If/else loop to print to the console whether or not these are the same employees
+            {
+                Console.WriteLine("These are the same employee.");
+            }
+            else
+            {
+                Console.WriteLine("These are two different employees.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
