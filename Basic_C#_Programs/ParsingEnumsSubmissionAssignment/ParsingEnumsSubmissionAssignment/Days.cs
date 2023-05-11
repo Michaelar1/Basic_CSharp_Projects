4219d9f1e94e0c41b00692b13c92a0c8f38372e4
Michaela Rehm <112276652+Michaelar1@users.noreply.github.com>
2023-05-11 15:00:38 -0400

    Updated TwentyOne and Created ParsingEnumsSubmissionAssignment

---
+++ Basic_C#_Programs/ParsingEnumsSubmissionAssignment/ParsingEnumsSubmissionAssignment/Days.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnumsSubmissionAssignment
+{
+    public enum Days  // Create an enum for the days of the week
+    {
+       Monday,
+       Tuesday,
+       Wednesday,
+       Thursday,
+       Friday,
+       Saturday,
+       Sunday
+    }
+}
---
+++ Basic_C#_Programs/ParsingEnumsSubmissionAssignment/ParsingEnumsSubmissionAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnumsSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the current day of the week: ");  // Prompts the user for the current day of the week
+            string dayInput = Console.ReadLine();  // Stores input as string variable dayInput
+
+            try // Wrap parsing in try/catch block
+            {
+                Days day = (Days)Enum.Parse(typeof(Days), dayInput);  // Converts the string to an enum
+            }
+            catch
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
