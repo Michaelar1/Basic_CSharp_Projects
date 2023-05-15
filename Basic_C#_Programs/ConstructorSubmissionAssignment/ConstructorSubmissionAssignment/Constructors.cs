af760bf00256ffbf9a95c5c5bb0e619bb9f616be
Michaela Rehm <112276652+Michaelar1@users.noreply.github.com>
2023-05-15 15:33:43 -0400

    Created ConstructorSubmissionAssignment

---
+++ Basic_C#_Programs/ConstructorSubmissionAssignment/ConstructorSubmissionAssignment/Constructors.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorSubmissionAssignment
+{
+    class Constructors
+    {
+        public string name;
+        public string address;
+        public void Person(string name) : this(address, "739 Willet Rd")
+        {
+
+        }
+        public void Person(string name, string address)
+        {
+            this.name = name;
+            this.address = address;
+        }
+    }
+}
---
+++ Basic_C#_Programs/ConstructorSubmissionAssignment/ConstructorSubmissionAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string myName = "Michaela";
+            Console.WriteLine(myName);
+            var number = 15.25;
+            Console.WriteLine(number);
+
+            Console.ReadLine();
+        }
+    }
+}
